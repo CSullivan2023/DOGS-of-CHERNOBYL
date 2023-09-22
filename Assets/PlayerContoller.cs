d5eca3a14f5dd4df643fe4beb4725383464051e5
CSullivan2023 <145032366+CSullivan2023@users.noreply.github.com>
2023-09-22 11:16:08 +0100

    second push

--- Assets/PlayerContoller.cs
+++ Assets/PlayerContoller.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class PlayerContoller : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D playerObject;
+
+    //Start is called before the firstframe update
     void Start()
     {
-        
+        //Find the Rigidbody2D component that is attached to the same object as this script
+        playerObject = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Create a 'float' that will be equal to the players horizontal input 
+        float movementValueX = Input.GetAxis('Horizontal');
+
+        //Change the X velocity of the Rigidbody2D to be equal to the movement value 
+        playerObject.velocity = new Vector2 (movementValueX, playerObject.velocity.y);
     }
 }
