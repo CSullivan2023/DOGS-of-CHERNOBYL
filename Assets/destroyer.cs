9295626d59efe22ab6deace99debadf208417808
CSullivan2023 <145032366+CSullivan2023@users.noreply.github.com>
2023-10-06 11:44:20 +0100

    new spawner code

--- Assets/destroyer.cs
+++ Assets/destroyer.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class destroyer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //much like like thew ()Start and ()update methods, onTriggerEnter2D is special unity method that is called
+    //by unity automatically at a specific point- in this case, when something enters the Trigger attached 
+    //to this game 
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        //if the gameobject that has collided with our trigger is tagged with cleanup...
+       if (collision.gameObject.tag == "CleanUp")
+        {
+            //then we use this method to destory it 
+            Destroy(collision.gameObject);
+        }
     }
 
     // Update is called once per frame
