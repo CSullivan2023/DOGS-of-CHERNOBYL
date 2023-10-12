12ad80c4f2c940936fad16a230a64d2229bb80e8
CSullivan2023 <145032366+CSullivan2023@users.noreply.github.com>
2023-10-12 11:27:08 +0100

    finishing button  worky

--- Assets/Scenes/button worky.cs
+++ Assets/Scenes/button worky.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class buttonworky : MonoBehaviour {
 
 
     public void playGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
-    // Start is called before the first frame update
+  
     void Start()
     {
         
