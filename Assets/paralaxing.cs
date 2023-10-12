b267f3bcdf71fbd608dcee964537cd77a743bfe0
CSullivan2023 <145032366+CSullivan2023@users.noreply.github.com>
2023-10-12 12:43:36 +0100

    update parallaxing

--- Assets/paralaxing.cs
+++ Assets/paralaxing.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class paralaxing : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float lengh, startpos;
+    public GameObject cam;
+    public float ParallaxEffect;
+
     void Start()
     {
+        startpos = transform.position.x;
+        lengh = GetComponent<SpriteRenderer>().bounds.size.x;
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
+        float temp = (cam.transform.position.x * (1 - ParallaxEffect));
+        float dist = (cam.transform.position.x * ParallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp < startpos + lengh)
         
     }
 }
